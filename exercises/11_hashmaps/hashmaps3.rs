fdb245a783b87ce8433a25c0564fb1d103bcaa18
oxwazz <muhammad.rahmahalim@gmail.com>
2024-11-13 20:40:39 +0700

    feat: learn rustling 11

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -9,7 +9,7 @@
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
-#[derive(Default)]
+#[derive(Debug)]
 struct TeamScores {
     goals_scored: u8,
     goals_conceded: u8,
@@ -31,6 +31,12 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        let tes1 = scores.entry(team_1_name).or_insert(TeamScores { goals_scored: 0, goals_conceded: 0});
+        (*tes1).goals_scored += team_1_score;
+        (*tes1).goals_conceded += team_2_score;
+        let tes2 = scores.entry(team_2_name).or_insert(TeamScores { goals_scored: 0, goals_conceded: 0});
+        (*tes2).goals_scored += team_2_score;
+        (*tes2).goals_conceded += team_1_score;
     }
 
     scores
@@ -62,6 +68,7 @@ England,Spain,1,0";
     #[test]
     fn validate_team_score_1() {
         let scores = build_scores_table(RESULTS);
+        dbg!(&scores);
         let team = scores.get("England").unwrap();
         assert_eq!(team.goals_scored, 6);
         assert_eq!(team.goals_conceded, 4);
